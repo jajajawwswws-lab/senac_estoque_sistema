0f8ff9af82f0c31045e1cb8a7db3788dd1be0572
jajajawwswws-lab <jajajawwswws@gmail.com>
2026-03-02 17:00:56 -0500

    Update backend.ts

--- api/backend.ts
+++ api/backend.ts
@@ -1,10 +1,7 @@
 // api/backend.ts
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import ServerRequest from './ServerRequest';
+import ServerRequest from './ServerRequest'; // Importa corretamente o default
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  // Apenas repassa para ServerRequest
   return ServerRequest(req, res);
 }
-
-//ss
