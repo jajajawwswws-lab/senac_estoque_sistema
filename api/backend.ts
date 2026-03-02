9e5622790fe02cf2445e00a95ba5a1515c0472a1
jajajawwswws-lab <jajajawwswws@gmail.com>
2026-03-02 20:12:30 -0300

    Update backend.ts
    ww

--- api/backend.ts
+++ api/backend.ts
@@ -1,9 +1,10 @@
 // api/backend.ts
 import { VercelRequest, VercelResponse } from '@vercel/node';
 //import ServerRequest from './ServerRequest'; // Certifique-se de que o arquivo existe e está com S maiúsculo
-
+//not vercel ->
 // Handler principal exportado para Vercel / Render
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   return ServerRequest(req, res);
 }
+
 
