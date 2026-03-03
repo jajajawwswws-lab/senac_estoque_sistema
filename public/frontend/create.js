067b1bfcdb1a5212791b69f9cf30307528e600d6
jajajawwswws-lab <jajajawwswws@gmail.com>
2026-03-03 15:22:21 -0500

    Update create.js

--- public/frontend/create.js
+++ public/frontend/create.js
@@ -6,7 +6,7 @@ window.onSubmit = function(token) {
     // A implementação será sobrescrita dentro do DOMContentLoaded
     // mas isso garante que a função existe quando o reCAPTCHA carregar
 };
-
+//l
 document.addEventListener('DOMContentLoaded', function() {
     // Elementos do DOM
     const form = document.getElementById('registrationForm');
@@ -413,3 +413,4 @@ document.addEventListener('DOMContentLoaded', function() {
         confirmPasswordInput.dispatchEvent(new Event('input'));
     }, 100);
 });
+
